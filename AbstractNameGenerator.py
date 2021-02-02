3cdc0eef9e89ee8cfeaa06db657eb22f652cc7fa
Jean-LouisH <LouisLou512.e@gmail.com>
2021-02-02 16:37:55 -0500

    Optimized algorithm, allows random syllable count, randomizes print by default, prints based on english language pronunciation

--- AbstractNameGenerator.py
+++ AbstractNameGenerator.py
@@ -1,117 +1,125 @@
 import random
 
-def command_print(name_output_list, tokens):
-        quantity = 0
-        if len(tokens) >= 2 and len(tokens) <= 3:
-                if tokens[1] == "all":
-                        quantity = len(name_output_list)
+RANDOM_SYLLABLE_COUNT = -1
+
+def print_names(quantity, syllables_count, min_syllables_count, max_syllables_count):
+        
+        # The following consonants were removed; 'c' for 'k' or 's'; 'q' for 'k'
+        # Other consonant sounds that are not suited for both beginning and ending are also removed
+        
+        consonant_sounds = ["b", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "r", "s", "t",
+                            "v", "w", "y", "z", "ch", "sk", "sh", "sm", "sp", "st", "th"]
+        beginning_only_consonant_sounds = []
+        end_only_consonant_sounds = []
+        vowel_sounds = ["ay", "ee", "eye", "oh", "oo", "aa", "eh", "ih", "ah", "uh", "aw"]
+        
+        print(" ")
+        for current_name_count in range(0, quantity, 1):
+                name = ""
+                
+                if syllables_count == RANDOM_SYLLABLE_COUNT:
+                        syllables_loop_count = random.randint(min_syllables_count, max_syllables_count)
                 else:
-                        try:
-                                quantity = int(tokens[1])
-                                if quantity < 1:
-                                        print("The number given is not more than 0")
-                                elif quantity > len(name_output_list):
-                                        quantity = len(name_output_list)
-                        except:
-                                print("The value '" + tokens[1] + "' is not a valid quantifier")
+                        syllables_loop_count = syllables_count
+                        
+                for syllable in range (0, syllables_loop_count, 1):
 
-                if quantity > 0:
-                        if len(tokens) == 2:
-                                print(" ")
-                                for i in range(0, quantity, 1):
-                                        print("\t" + name_output_list[i])
-                        elif len(tokens) == 3:
-                                if tokens[2] == "shuffled":
-                                        shuffled_names = name_output_list
-                                        random.shuffle(shuffled_names)
-                                        print(" ")
-                                        for i in range(0, quantity, 1):
-                                                print("\t" + shuffled_names[i])
-                                else:
-                                        print("'" + tokens[2] + "' is not a valid parameter")
+                        #Every syllable has 0 or 1 beginning consonants
+                        consonant_beginning_count = random.randint(0, 1)
+                        
+                        for j in range(0, consonant_beginning_count, 1):
+                                consonant_index = random.randint(0, len(consonant_sounds) - 1)
+                                name += consonant_sounds[consonant_index]
+                                if j == consonant_beginning_count - 1:
+                                        name += "-"
 
-def command_syllables(tokens):
+                        #Every syllable has 1 vowel
+                        vowel_index = random.randint(0, len(vowel_sounds) - 1)
+                        name += vowel_sounds[vowel_index]
+
+                        #Every syllable has 0 to 2 ending consonants
+                        consonant_end_count = random.randint(0, 2)
+                        for j in range(0, consonant_end_count, 1):
+                                if j == 0:
+                                        name += "-"
+                                consonant_index = random.randint(0, len(consonant_sounds) - 1)
+                                name += consonant_sounds[consonant_index]
+
+                        if syllable != syllables_loop_count - 1:
+                                name += "-"
+                                
+                print("\t" + name)
+                
+                        
+def command_print(tokens, syllables_count, min_syllables_count, max_syllables_count):
+        quantity = 0
+        
         if len(tokens) == 2:
                 try:
                         quantity = int(tokens[1])
-                        if quantity >= 1:
-                                return quantity
-                        else:
+                        if quantity < 1:
                                 print("The number given is not more than 0")
                 except:
-                        print("The parameter '" + tokens[1] + "' is not an integer")
+                        print("The value '" + tokens[1] + "' is not a valid quantifier")
+
+                if quantity >= 1:
+                        print_names(quantity, syllables_count, min_syllables_count, max_syllables_count)
         else:
                 print("This command has too many parameters")
+
+def command_min_max_syllables(tokens):
+        min_max_syllables = [1, 2]
+        
+        if len(tokens) == 3:
+                for i in range(0, 2, 1):
+                        quantity = int(tokens[i + 1])
+                        if quantity >= 1:
+                                min_max_syllables[i] = quantity
+                        else:
+                                print("The number given is not more than 0")
+                                min_max_syllables[i] = 1
+
+                if min_max_syllables[0] >=  min_max_syllables[1]:
+                        min_max_syllables[1] = min_max_syllables[0] + 1
+
+                return min_max_syllables
+        else:
+                print("This command has too many or too little parameters")
+
+
+def command_syllables(tokens):
+        if len(tokens) == 2:
+                if tokens[1] == "random":
+                        return RANDOM_SYLLABLE_COUNT
+                else:
+                        try:
+                                quantity = int(tokens[1])
+                                if quantity >= 1:
+                                        return quantity
+                                else:
+                                        print("The number given is not more than 0")
+                        except:
+                                print("The parameter '" + tokens[1] + "' is not an integer")
+        else:
+                print("This command has too many parameters")
+
+def get_command_examples():
+        return "'syllables 4' \n'syllables random' \n'min_max_syllables 2 3' \n'print 5'"
+
+def get_supported_commands():
+        return "'exit' \n'commands' \n'syllables <quantity>' \n'print <quantity>'"
 
 def command_commands():
         print("\nMenu commands;\n\n" + get_supported_commands())
         print("\nCommand examples;\n\n" + get_command_examples())
-        print("\nNote: 1 to 3 syllables is recommended, anymore will take several minutes or more to process. The default at startup is 3.")
-
-def convert_to_name(syllable_list, syllable_index_list):
-        name_output = ""
-        
-        for i in syllable_index_list:
-                name_output += syllable_list[i]
-
-        return name_output
-
-def are_all_indices_last(syllabary_count, syllable_index_list):
-        return all(index == syllabary_count - 1 for index in syllable_index_list)
-
-def create_name_output_list(syllable_list, syllable_index_list):
-        syllable_count = len(syllable_index_list)
-        syllabary_count = len(syllable_list)
-        name_output_list = []
-        
-        while not are_all_indices_last(syllabary_count, syllable_index_list):
-                name_output_list.append(convert_to_name(syllable_list, syllable_index_list))
-                syllable_index_list[syllable_count - 1] += 1
-                
-                for i in range(syllable_count - 1, -1, -1):
-                        if syllable_index_list[i] > syllabary_count - 1 and i - 1 >= 0:
-                                syllable_index_list[i] = 0
-                                syllable_index_list[i - 1] += 1
-
-        name_output_list.append(convert_to_name(syllable_list, syllable_index_list))
-        
-        return name_output_list
-
-def get_command_examples():
-        return "'syllables 4' \n'print 5' \n'print 3 shuffled' \n'print all'"
-
-def get_supported_commands():
-        return "'exit' \n'commands' \n'syllables <quantity>' \n'print <quantity> (shuffled)'"
-
-def create_syllable_index_list(syllables_count):
-        syllable_index_list = []
-        
-        for i in range(syllables_count):
-                syllable_index_list.append(0)
-
-        return syllable_index_list
-
-def create_syllable_list():
-        syllable_list = []
-        consonant_letters = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z"]
-        consonants = consonant_letters
-        vowels = ["a", "i", "u", "e", "o"]
-        
-        for consonant in consonants:
-                for vowel in vowels:
-                        syllable_list.append(consonant + vowel)
-
-        for vowel in vowels:
-                syllable_list.append(vowel)
-
-        return syllable_list
+        print("\nNote: The default syllable count is random.")
 
 def main():
-        syllable_list = create_syllable_list()
+        syllables_count = RANDOM_SYLLABLE_COUNT
+        min_syllables_count = 1
+        max_syllables_count = 4
+
         print("\t\tAbstract Name Generator")
-        syllables_count = 3
-        syllable_index_list = create_syllable_index_list(syllables_count)
-        name_output_list = create_name_output_list(syllable_list, syllable_index_list)
         command_commands()
 
         menu_input = ""
@@ -124,10 +132,12 @@ def main():
                 
                 if command == "syllables":
                         syllables_count = command_syllables(tokens)
-                        syllable_index_list = create_syllable_index_list(syllables_count)
-                        name_output_list = create_name_output_list(syllable_list, syllable_index_list)
+                elif command == "min_max_syllables":
+                        min_max_syllables = command_min_max_syllables(tokens)
+                        min_syllables = min_max_syllables[0]
+                        max_syllables = min_max_syllables[1]
                 elif command == "print":
-                        command_print(name_output_list, tokens)
+                        command_print(tokens, syllables_count, min_syllables_count, max_syllables_count)
                 elif command == "exit":
                         is_exiting = True
                 elif command == "commands":
